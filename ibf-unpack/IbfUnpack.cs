b05f262782b93631e6d743f9f21117dc8f74c30f
pin37 <ucorpor@gmail.com>
2023-04-16 20:07:53 +0400

    refactoring

--- ibf-unpack/IbfUnpack.cs
+++ ibf-unpack/IbfUnpack.cs
@@ -30,29 +30,32 @@ namespace ibf_unpack
                 string filePath = Path.Combine(systemDir, fileName);
                 File.WriteAllText(filePath, string.Empty);
 
-                byte[] fileLengthBytes = new byte[4];
-                stream.Read(fileLengthBytes, 0, fileLengthBytes.Length);
-                int fileLenght = BitConverter.ToInt32(fileLengthBytes, 0);
-
-                int chunkSize = 1024;
+                int chunkSize = 2048;
+                int fileLenght = BitConverter.ToInt32(ReadBytes(stream, 4), 0);
                 for (int i = chunkSize; i < fileLenght; i += chunkSize)
                 {
-                    WriteChunk(stream, chunkSize, filePath);
+                    WriteChunkFromStream(stream, chunkSize, filePath);
                 }
 
                 int remained = fileLenght - fileLenght / chunkSize * chunkSize;
-                WriteChunk(stream, remained, filePath);
+                WriteChunkFromStream(stream, remained, filePath);
 
                 fileNameLength = stream.ReadByte();
             }
             stream.Close();
         }
 
-        private static void WriteChunk(Stream stream, int size, string filePath)
+        private static byte[] ReadBytes(Stream stream, int length)
         {
-            byte[] chunkBytes = new byte[size];
-            stream.Read(chunkBytes, 0, chunkBytes.Length);
-            string chunk = Encoding.UTF8.GetString(chunkBytes);
+            byte[] bytesArray = new byte[length];
+            stream.Read(bytesArray, 0, length);
+            return bytesArray;
+        }
+
+        private static void WriteChunkFromStream(Stream stream, int size, string filePath)
+        {
+            byte[] bytesArray = ReadBytes(stream, size);
+            string chunk = Encoding.UTF8.GetString(bytesArray);
             File.AppendAllText(filePath, chunk);
         }
 
