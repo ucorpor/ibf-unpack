51b5d2e3607aac7a87c19319b60bc1e979092ee5
pin37 <ucorpor@gmail.com>
2023-04-16 21:39:05 +0400

    main form default position in center

--- ibf-unpack/MainForm.Designer.cs
+++ ibf-unpack/MainForm.Designer.cs
@@ -96,6 +96,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ibf-unpack";
             this.ResumeLayout(false);
             this.PerformLayout();
